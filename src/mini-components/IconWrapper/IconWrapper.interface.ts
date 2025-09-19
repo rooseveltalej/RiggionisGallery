35c1662d79135da6e872a3322d7b363f6a6a7165
rooseveltalej <roperez@estudiantec.cr>
2025-09-19 15:02:05 -0600

    feat: Add IconWrapper component with props interface and styling

---
+++ src/mini-components/IconWrapper/IconWrapper.interface.ts
@@ -0,0 +1,8 @@
+import type React from "react";
+
+export interface IconWrapperProps extends React.SVGProps<SVGSVGElement> {
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  size?: React.CSSProperties["width"];
+  color?: React.CSSProperties["color"];
+  className?: string;
+}
