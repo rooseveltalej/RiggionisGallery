fb4298ae61a9c8214d38738c203931195610c1de
mortalcr <daporrasn@gmail.com>
2025-09-19 00:02:28 -0600

    refactor: use CSSWithVars type for custom properties in Button component

--- src/mini-components/Button/Button.tsx
+++ src/mini-components/Button/Button.tsx
@@ -3,19 +3,23 @@ import React from "react";
 import "./Button.css";
 import type { ButtonProps } from "./types";
 
+type CSSWithVars = React.CSSProperties & {
+  [key: `--${string}`]: string | number;
+};
+
 const Button: React.FC<ButtonProps> = ({
   text,
   icon,
   color = "#5c0a0a",
-  onClick,
+  ...rest
 }) => {
+  const style: CSSWithVars = {
+    "--btn-bg": color,
+  };
+
   return (
-    <button
-      onClick={onClick}
-      className="button"
-      style={{ ["--btn-bg" as any]: color }}
-    >
-      {icon && <span>{icon}</span>}
+    <button className="button" style={style} {...rest}>
+      {icon && <span className="button-icon">{icon}</span>}
       <span>{text}</span>
     </button>
   );
