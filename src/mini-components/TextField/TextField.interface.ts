948c4de88dfb8a78594129af7b6b9f3c4adb82f4
rooseveltalej <roperez@estudiantec.cr>
2025-09-19 15:02:18 -0600

    feat: Update TextField component with improved styling and props handling

--- src/mini-components/TextField/TextField.interface.ts
+++ src/mini-components/TextField/TextField.interface.ts
@@ -2,7 +2,6 @@
 import type { InputHTMLAttributes } from "react";
 
 export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-	placeholder?: string;
 	icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-	width?: string | number;
+	width?: React.CSSProperties["width"];
 }
--- src/mini-components/TextField/TextField.tsx
+++ src/mini-components/TextField/TextField.tsx
@@ -1,18 +1,18 @@
 import React, { type FC } from "react";
 import "./TextField.css";
+import IconWrapper from "../IconWrapper/IconWrapper";
 
 import type { TextFieldProps } from "./TextField.interface";
 
 
-const TextField: FC<TextFieldProps> = ({ placeholder, icon: Icon, width}) => {
+const TextField: FC<TextFieldProps> = ({ icon, width, ...props }) => {
   const style = width ? { width } : undefined;
   return (
     <div className="search-field" style={style}>
-      {Icon && <Icon className="search-icon" />}
+      {icon && <IconWrapper icon={icon} className="search-icon" />}
       <input
-        type="text"
-        placeholder={placeholder}
         className="search-input"
+        {...props}
       />
     </div>
   );
