5c2d55874d29d5cc0007bb91b9ceca2657495557
rooseveltalej <roperez@estudiantec.cr>
2025-09-19 14:26:07 -0600

    feat: Update Paragraph component to use children prop instead of text

--- src/mini-components/paragraph/Paragraph.tsx
+++ src/mini-components/paragraph/Paragraph.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import type { ParagraphProps } from "./paragraph.interface";
 
-
 const Paragraph: React.FC<ParagraphProps> = ({
-  text,
+  children,
   color,
   fontSize,
   fontWeight,
@@ -17,7 +16,7 @@ const Paragraph: React.FC<ParagraphProps> = ({
 
   return (
     <p className={`paragraph ${className}`.trim()} style={style}>
-      {text}
+      {children}
     </p>
   );
 };
