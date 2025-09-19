dde447e8c5321d6d3a0aaef931fea50cd1d80a87
mortalcr <daporrasn@gmail.com>
2025-09-19 00:01:04 -0600

    feat: extend ButtonProps with native HTML button attributes

--- src/mini-components/Button/types.ts
+++ src/mini-components/Button/types.ts
@@ -1,9 +1,8 @@
 // src/mini-components/Button/types.ts
 import React from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   text: string;
   icon?: React.ReactNode;
   color?: string;
-  onClick?: () => void;
 }
