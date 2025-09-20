a15c45c7ffe8324b7054fb5d3900fccebf8f9d77
rooseveltalej <roperez@estudiantec.cr>
2025-09-20 08:40:13 -0600

    feat: Enhance TextField component with improved props handling and add constants for better maintainability

--- src/mini-components/TextField/TextField.interface.ts
+++ src/mini-components/TextField/TextField.interface.ts
@@ -3,4 +3,5 @@ import type { InputHTMLAttributes } from "react";
 export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 	icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 	width?: React.CSSProperties["width"];
+	htmlFor?: string;
 }
--- src/mini-components/TextField/TextField.tsx
+++ src/mini-components/TextField/TextField.tsx
@@ -1,25 +1,42 @@
 import React, { type FC } from "react";
 import "./TextField.css";
-import IconWrapper from "../IconWrapper/IconWrapper";
+import IconWrapper from "@/mini-components/IconWrapper/IconWrapper";
+import { generateId } from "@/utils/idGenerator";
+import { DEFAULT_TEXTS, CSS_CLASSES, ID_PREFIXES } from "@/utils/constants/textField.constants";
 
 import type { TextFieldProps } from "./TextField.interface";
+
 
 
 const TextField: FC<TextFieldProps> = ({ 
   icon, 
   width, 
   className = "",
+  htmlFor,
+  id,
+  title,
   ...props 
 }) => {
+  const inputId = id ?? generateId(ID_PREFIXES.TEXTFIELD);
+
   return (
     <div 
-      className={`search-field ${className ?? ""}`.trim()}
+      className={`${CSS_CLASSES.CONTAINER} ${className ?? DEFAULT_TEXTS.EMPTY}`.trim()}
       style={width ? { width } : undefined}
     >
-      {icon && <IconWrapper icon={icon} className="search-icon" />}
+      {htmlFor && (
+        <label htmlFor={inputId} className={CSS_CLASSES.LABEL}>
+          {title ?? DEFAULT_TEXTS.LABEL_FALLBACK} {/* todo: this need to be implemented with remote_config */}
+        </label>
+      )}
+
+      {icon && <IconWrapper icon={icon} className={CSS_CLASSES.ICON} />}
+
       <input
-        className="search-input"
-        {...props}
+        id={inputId}
+        className={CSS_CLASSES.INPUT}
+        title={title}
+        {...props} // this includes placeholder, type, aria-label, etc.
       />
     </div>
   );
