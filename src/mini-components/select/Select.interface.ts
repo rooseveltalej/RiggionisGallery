7c91515c90ffa83b527e63effab55e0b03c3657b
Kevin Jiménez <kvn.jimenez.t@gmail.com>
2025-09-23 14:55:13 -0600

    Update: improve interface for disable and icon

--- src/mini-components/select/Select.interface.ts
+++ src/mini-components/select/Select.interface.ts
@@ -1,6 +1,13 @@
-import type { SelectHTMLAttributes } from 'react';
+import type { SelectHTMLAttributes, ReactNode } from 'react';
+
+export interface Option {
+  value: string;
+  label: string;
+  disabled?: boolean;
+  icon?: ReactNode;
+}
 
 export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
-  options: { value: string; label: string }[];
+  options: Option[];
   placeholder?: string;
 }
