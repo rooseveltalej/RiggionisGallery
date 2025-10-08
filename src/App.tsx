31392dbfac6659d055bdda5a854f906d3143cdf9
Kevin Jiménez <kvn.jimenez.t@gmail.com>
2025-10-08 12:43:12 -0600

    update: Clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { useLanguage } from '@/hooks';
 import {Footer} from '@/components';
-import { H1 } from '@/mini-components';
 
 function App() {
   const { languageStrings, loading } = useLanguage();
@@ -12,12 +11,8 @@ function App() {
 
   return (
     <div className="app">
-      <header>
-        <H1>{languageStrings.navbar?.routes?.gallery}</H1>
-      </header>
-
       <main className="main-content">
-        {/* Tu contenido principal */}
+
       </main>
 
       <Footer 
