231d19ddbf15bccb7afeb5fd3156474b3dd25919
Anthony Jimenez <noni45100@hotmail.com>
2025-11-02 19:05:44 -0600

    Refactor LanguageSelector: simplificar el uso de estilos mediante constantes

--- src/components/languageSelector/LanguageSelector.tsx
+++ src/components/languageSelector/LanguageSelector.tsx
@@ -3,6 +3,9 @@ import { useLanguage } from "@/hooks";
 import Select from "@/mini-components/select/Select";
 import styles from "@/components/languageSelector/LanguageSelector.module.css";
 
+const languageSelectorWrapper = styles["language-selector-wrapper"];
+const languageSelectorSelect = styles["language-selector-select"];
+
 const LanguageSelector: React.FC = () => {
   const { language, availableLanguages, languageStrings } = useLanguage();
   const selectOptions = availableLanguages.map((lang) => ({
@@ -11,11 +14,11 @@ const LanguageSelector: React.FC = () => {
   }));
 
   return (
-    <div className={styles["language-selector-wrapper"]}>
+    <div className={languageSelectorWrapper}>
       <Select
         options={selectOptions}
         value={language}
-        className={styles["language-selector-select"]}
+        className={languageSelectorSelect}
         style={{ minWidth: 100 }}
       />
     </div>
