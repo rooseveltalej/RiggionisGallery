8360921c6a52a70cfd996d4dc3eb22bf416b47d8
Anthony Jimenez <noni45100@hotmail.com>
2025-11-02 19:06:41 -0600

    Refactor LanguageSelector: optimizar la creación de opciones de selección utilizando useMemo

--- src/components/languageSelector/LanguageSelector.tsx
+++ src/components/languageSelector/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useLanguage } from "@/hooks";
 import Select from "@/mini-components/select/Select";
 import styles from "@/components/languageSelector/LanguageSelector.module.css";
@@ -8,10 +8,15 @@ const languageSelectorSelect = styles["language-selector-select"];
 
 const LanguageSelector: React.FC = () => {
   const { language, availableLanguages, languageStrings } = useLanguage();
-  const selectOptions = availableLanguages.map((lang) => ({
-    value: lang,
-    label: languageStrings?.navbar?.languages?.[lang] || lang,
-  }));
+
+  const selectOptions = useMemo(
+    () =>
+      availableLanguages.map((lang) => ({
+        value: lang,
+        label: languageStrings?.navbar?.languages?.[lang] || lang,
+      })),
+    [availableLanguages, languageStrings]
+  );
 
   return (
     <div className={languageSelectorWrapper}>
