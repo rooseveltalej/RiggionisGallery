d131439e15e96103d75b9e6ed4f28ddc44c6da68
Anthony Jimenez <noni45100@hotmail.com>
2025-11-02 19:02:20 -0600

    Refactor LanguageSelector: eliminar código comentado y mejorar la legibilidad

--- src/components/languageSelector/LanguageSelector.tsx
+++ src/components/languageSelector/LanguageSelector.tsx
@@ -4,23 +4,17 @@ import Select from "@/mini-components/select/Select";
 import styles from "@/components/languageSelector/LanguageSelector.module.css";
 
 const LanguageSelector: React.FC = () => {
-  const { language, availableLanguages, languageStrings } =
-    useLanguage();
+  const { language, availableLanguages, languageStrings } = useLanguage();
   const selectOptions = availableLanguages.map((lang) => ({
     value: lang,
     label: languageStrings?.navbar?.languages?.[lang] || lang,
   }));
-
-  // const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  //   setLanguage(e.target.value);
-  // };
 
   return (
     <div className={styles["language-selector-wrapper"]}>
       <Select
         options={selectOptions}
         value={language}
-        // onChange={handleLanguageChange}
         className={styles["language-selector-select"]}
         style={{ minWidth: 100 }}
       />
