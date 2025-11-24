44d2b5865dacf1ef84b6117797d5e5d2d9c875b8
Walter Lazo <walterlazo293@gmail.com>
2025-11-24 17:57:45 -0600

    Update Index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,4 +3,5 @@ export { useWhatsAppLink } from './useWhatsAppLink';
 export { useProjectActions } from './useProjectActions';
 export { useContactData } from './useContactData';
 export { useFavorites } from './useFavorites';
+export { useSimilarProjects } from './useSimilarProjects';
 export { useProjectLikesCount } from './useProjectLikesCount';
