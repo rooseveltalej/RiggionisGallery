d748c772827675a8ccefcb496880675c7008c499
Khraben <kvn.jimenez.t@gmail.com>
2025-11-25 16:20:40 -0600

    update: move icons

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,26 +1,26 @@
 // Constants for the Contact page
 
 export const AVATAR_NAME = "Mónica Riggioni";
-export const AVATAR_IMAGE_URL = "/src/assets/images/logo/Blanco - Vino.png";
+export const AVATAR_IMAGE_URL = "/images/logo/Blanco - Vino.png";
 
 export const CONTACT_PHONE = "+506 0000 0000";
 export const CONTACT_EMAILS = ["correo1@gmail.com", "correo2@gmail.com"];
 
 export const SOCIAL_LINKS = [
   {
-    iconSrc: "/src/assets/icons/instagram.svg",
+    iconSrc: "/icons/instagram.svg",
     alt: "Instagram",
     href: "https://instagram.com/usuario",
     label: "Visitar perfil de Instagram",
   },
   {
-    iconSrc: "/src/assets/icons/facebook.svg",
+    iconSrc: "/icons/facebook.svg",
     alt: "Facebook",
     href: "https://facebook.com/usuario",
     label: "Visitar perfil de Facebook",
   },
   {
-    iconSrc: "/src/assets/icons/linkedin.svg",
+    iconSrc: "/icons/linkedin.svg",
     alt: "Linkedin",
     href: "https://linkedin.com/in/usuario",
     label: "Visitar perfil de LinkedIn",
@@ -28,4 +28,4 @@ export const SOCIAL_LINKS = [
 ];
 
 export const WHATSAPP_BUTTON_TEXT = "Enviar mensaje";
-export const WHATSAPP_ICON_SRC = "/src/assets/icons/whatsapp.svg";
+export const WHATSAPP_ICON_SRC = "/icons/whatsapp.svg";
